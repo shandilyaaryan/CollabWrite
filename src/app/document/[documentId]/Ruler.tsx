c06b20804ee3deb2f7222df44044d4377e685f4b
shandilyaaryan <aryanshandilya10@gmail.com>
2024-12-12 09:18:51 +0000

    Added Ruler

---
+++ src/app/document/[documentId]/Ruler.tsx
@@ -0,0 +1,80 @@
+import { FaCaretDown } from "react-icons/fa"
+
+const markers = Array.from({ length: 83 }, (_, i) => i);
+
+export const Ruler = () => {
+    return (
+        <div className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+            <div 
+            id="ruler-container"
+            className="max-w-[816px] mx-auto w-full h-full relative "
+            >
+            <Marker 
+            position={56}
+            isLeft={true}
+            isdragging={false}
+            onMouseDown={() => {}}
+            onDoubleClick={() => {}}
+            />
+                <div className="absolute inset-x-0 bottom-0 h-full">
+                    <div className="relative h-full w-[816px]">
+                        {markers.map((marker) => {
+                            const position = (marker * 816) / 82;
+
+                            return (
+                                <div
+                                key={marker}
+                                className="absolute bottom-0"
+                                style={{ left: `${position}px`}}
+                                >
+                                    {marker % 10 === 0 && (
+                                        <>
+                                            <div className="absolute bottom-0 w-[1px] h-2 bg-neutral-500">
+                                                <span className="absolute bottom-2 text-[10px] text-neutral-500 transform -translate-x-1/2">
+                                                    {marker / 10 + 1}
+                                                </span>
+                                            </div>
+                                        </>
+                                    )}
+                                    {marker % 5 === 0 && marker % 10 != 0 && (
+                                        <div className="absolute bottom-0 w-[1px] h-1.5 bg-neutral-500" />
+                                    )}
+                                    {marker % 5 != 0 && (
+                                        <div className="absolute bottom-0 w-[1px] h-1 bg-neutral-500" />
+                                    )}
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+interface MarkerProps {
+    position: number;
+    isLeft: boolean;
+    isdragging: boolean;
+    onMouseDown: () => void;
+    onDoubleClick: () => void;
+};
+
+const Marker = ({
+    position,
+    isLeft,
+    isdragging,
+    onDoubleClick,
+    onMouseDown
+}: MarkerProps) => {
+    return(
+    <div
+    className="absolute top-0 w-4 h-full cursor-ew-resize z-[5] group -ml-2"
+    style={{ [isLeft ? "left" : "right"]: `${position}px`}}
+    onMouseDown={onMouseDown}
+    onDoubleClick={onDoubleClick}
+    >
+        <FaCaretDown className="absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2" />
+    </div>
+)}
--- src/app/document/[documentId]/editor.tsx
+++ src/app/document/[documentId]/editor.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
+import { Ruler } from "./Ruler";
 import StarterKit from "@tiptap/starter-kit";
 import TaskItem from "@tiptap/extension-task-item";
 import FontFamily from "@tiptap/extension-font-family";
@@ -26,6 +27,7 @@ export const Editor = () => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -91,6 +93,7 @@ export const Editor = () => {
   });
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
