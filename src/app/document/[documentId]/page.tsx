c9a6ddd7798188fe2bdce0880e19b0abb52f992b
shandilyaaryan <aryanshandilya10@gmail.com>
2024-12-13 15:10:07 +0000

    Added navbar

--- src/app/document/[documentId]/page.tsx
+++ src/app/document/[documentId]/page.tsx
@@ -1,8 +1,10 @@
 import { Editor } from "./editor";
+import { Navbar } from "./navbar";
 import { Toolbar } from "./toolbar";
 const DocumentPage = async ( ) => {
     return ( 
         <div className="min-h-screen bg-[#FAFBFD]">
+            <Navbar /> 
             <Toolbar />
             <Editor />
         </div>
