53edf9f38adf2b2de0f4100e974593d8ab4bcd43
shandilyaaryan <aryanshandilya10@gmail.com>
2024-12-19 13:08:29 +0530

    Added Nuqs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google"
-
+import { NuqsAdapter } from "nuqs/adapters/next/app"
 import "./globals.css";
 
 
@@ -23,7 +23,9 @@ export default function RootLayout({
       <body
         className={inter.className}
       >
-        {children}
+        <NuqsAdapter>
+          {children}
+        </NuqsAdapter>
       </body>
     </html>
   );
