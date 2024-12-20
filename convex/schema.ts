22a3102b04f68bdc7959b43b18df844d233216ff
shandilyaaryan <aryanshandilya10@gmail.com>
2024-12-20 11:29:48 +0530

    Added Schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,6 +5,15 @@ import { v } from "convex/values";
 export default defineSchema({
     documents: defineTable({
         title: v.string(),
-        
+        initialContent: v.optional(v.string()),
+        ownerId: v.string(),
+        roomId: v.optional(v.string()),
+        organizationId: v.optional(v.string()),
     })
+     .index("by_owner_id", ["ownerId"])
+     .index("by_organization_id", ["organizationId"])
+     .searchIndex("search_title", {
+        searchField: "title",
+        filterFields: ["ownerId", "organizationId"]
+     })
 })
