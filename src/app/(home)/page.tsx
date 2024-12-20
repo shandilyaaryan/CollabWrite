c70a857f90462ae4ab6c86b13321053d56a2f616
shandilyaaryan <aryanshandilya10@gmail.com>
2024-12-20 10:33:17 +0530

    Integrated Database(Convex)

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useQuery } from "convex/react";
 import { Navbar } from "./Navbar";
 import { TemplateGallery } from "./templateGallery";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
+  if (documents === undefined) {
+    return (
+      <p className="w-screen h-[100vh] text-lg justify-center items-center">Loading...</p>
+    )
+  }
   return ( 
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +20,9 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((document) => (
+        <span key={document._id}>{document.title}</span>
+      ) )}
       </div>
     </div>
    );
