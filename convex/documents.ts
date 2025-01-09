e30f060598d7fd1449c0acd94b59bc710bf6996e
shandilyaaryan <aryanshandilya10@gmail.com>
2025-01-09 20:26:40 +0530

    Improved Organizations

--- convex/documents.ts
+++ convex/documents.ts
@@ -27,8 +27,20 @@ export const get = query({
       if (!user) {
         throw new ConvexError("Unauthorized")
       }
+
+      const organizationId = (user.organization_id?? undefined) as | string | undefined;
+      
+
+      if ( search && organizationId){
+        return await ctx.db.query("documents").withSearchIndex("search_title", (q) => q.search("title", search).eq("organizationId", organizationId)).paginate(paginationOpts)
+      }
+
       if (search) {
         return await ctx.db.query("documents").withSearchIndex("search_title", (q) => q.search("title", search).eq("ownerId", user.subject)).paginate(paginationOpts)
+      }
+
+      if (organizationId) {
+        return await ctx.db.query("documents").withIndex("by_organization_id", (q) => q.eq("organizationId", organizationId)).paginate(paginationOpts); 
       }
 
       return await ctx.db.query("documents").withIndex("by_owner_id", (q) => q.eq("ownerId", user.subject)).paginate(paginationOpts);
