a00fcf4900b60c862d2882ed810772e8818ce999
shandilyaaryan <aryanshandilya10@gmail.com>
2025-01-11 15:09:41 +0530

    UTH changes

--- src/app/document/[documentId]/page.tsx
+++ src/app/document/[documentId]/page.tsx
@@ -3,13 +3,7 @@ import { Navbar } from "./navbar";
 import { Room } from "./Room";
 import { Toolbar } from "./toolbar";
 
-interface DocumentIdPageProps {
-  params: Promise<{documentId: string}>
-}
-
-const DocumentPage = async ({ params }: DocumentIdPageProps) => {
-  throw new Error("OO")
-  const { documentId } = await params;
+const DocumentPage = async () => {
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
       <div className="flex z-10 flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 bg-[#FAFBFD] print:hidden">
