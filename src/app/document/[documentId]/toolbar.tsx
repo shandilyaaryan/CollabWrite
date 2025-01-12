c125174edf7acae08b44ded85eb3f19a9b1ecef9
shandilyaaryan <aryanshandilya10@gmail.com>
2025-01-12 20:02:37 +0530

    UTH Changes

--- src/app/document/[documentId]/toolbar.tsx
+++ src/app/document/[documentId]/toolbar.tsx
@@ -640,7 +640,7 @@ export const Toolbar = () => {
   ];
 
   return (
-    <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+    <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto -z-50">
       {sections[0].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
