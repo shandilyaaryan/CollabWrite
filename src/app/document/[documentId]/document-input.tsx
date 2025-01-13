8767af1b950af82759476f44d0bfbfed106c0cc2
shandilyaaryan <aryanshandilya10@gmail.com>
2025-01-13 17:43:04 +0530

    Improvised previous commit

--- src/app/document/[documentId]/document-input.tsx
+++ src/app/document/[documentId]/document-input.tsx
@@ -3,6 +3,8 @@ import { Id } from "../../../../convex/_generated/dataModel";
 import { useRef, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
+import { useDebounce } from "@/hooks/use-debounce";
+import { toast } from "sonner";
 
 interface DocumentInputProps {
   title: string;
@@ -19,15 +21,40 @@ export const DocumentInput = ({ title, id }: DocumentInputProps) => {
 
   const mutate = useMutation(api.documents.updateById);
 
+  const debouncedUpdate = useDebounce((newValue: string) => {
+    if (newValue === title) return;
+
+    setIsPending(true);
+    mutate({id, title: newValue})
+      .then(() => toast.success("Document Successfully updated"))
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsPending(false))
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsPending(true);
+    mutate({id, title: value})
+      .then(() => { 
+        toast.success("Document Successfully updated");
+        setIsEditng(false);
+    })
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsPending(false))
+  };
+
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
     setValue(newValue);
+    debouncedUpdate(newValue)
   }
 
   return (
     <div className="flex items-center gap-2">
       {isEditing ? (
-        <form className="relative w-fit max-w-[50ch]">
+        <form onSubmit={handleSubmit} className="relative w-fit max-w-[50ch]">
           <span className="invisible whitespace-pre px-1.5 text-lg">
             {value || " "}
           </span>
@@ -35,6 +62,7 @@ export const DocumentInput = ({ title, id }: DocumentInputProps) => {
             ref={inputRef}
             value={value}
             onChange={onChange}
+            onBlur={() => setIsEditng(false)}
             className="absolute inset-0 text-lg text-black px-1.5 bg-transparent truncate"
           />
         </form>
