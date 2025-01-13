8ed22ec0f28e7b79ecc8c3bf563f0b44c74ff7a5
shandilyaaryan <aryanshandilya10@gmail.com>
2025-01-13 12:45:19 +0530

    UTH changes

---
+++ src/app/document/[documentId]/document.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { api } from "../../../../convex/_generated/api";
+import { Preloaded, usePreloadedQuery } from "convex/react";
+import { Editor } from "./editor";
+import { Navbar } from "./navbar";
+import { Room } from "./Room";
+import { Toolbar } from "./toolbar";
+
+interface DocumentProps {
+  preloadedDocument: Preloaded<typeof api.documents.getById>;
+}
+
+export const Document = ({ preloadedDocument }: DocumentProps) => {
+    const document = usePreloadedQuery(preloadedDocument)
+  return (
+    <Room>
+      <div className="min-h-screen bg-[#FAFBFD]">
+        <div className="flex z-10 flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 bg-[#FAFBFD] print:hidden">
+          <Navbar data={document} />
+          <Toolbar />
+        </div>
+        <div className="pt-[114px] print:pt-0">
+          <Editor />
+        </div>
+      </div>
+    </Room>
+  );
+};
--- src/app/document/[documentId]/page.tsx
+++ src/app/document/[documentId]/page.tsx
@@ -1,22 +1,29 @@
-import { Editor } from "./editor";
-import { Navbar } from "./navbar";
-import { Room } from "./Room";
-import { Toolbar } from "./toolbar";
+import { auth } from "@clerk/nextjs/server";
+import { preloadQuery } from "convex/nextjs"
+import { Id } from "../../../../convex/_generated/dataModel";
+import { Document } from "./document";
+import { api } from "../../../../convex/_generated/api";
 
-const DocumentPage = async () => {
-  return (
-  <Room>
-    <div className="min-h-screen bg-[#FAFBFD]">
-      <div className="flex z-10 flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 bg-[#FAFBFD] print:hidden">
-        <Navbar />
-        <Toolbar />
-      </div>
-      <div className="pt-[114px] print:pt-0">
-        <Editor />
-      </div>
-    </div>
-  </Room>
-  );
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: Id<"documents">}>;
 };
 
-export default DocumentPage;
+const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
+
+  const { getToken } = await auth();
+  const token = await getToken({ template: "convex"}) ?? undefined;
+
+  if (!token) {
+    throw new Error("Unauthorized")
+  }
+
+  const preloadedDocument = await preloadQuery(
+    api.documents.getById,
+    { id: documentId },
+    { token }
+  )
+  return <Document preloadedDocument={preloadedDocument} />;
+};
+
+export default DocumentIdPage;
