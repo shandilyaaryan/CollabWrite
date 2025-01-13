f00e59228c71e8de3daa1b8e40df27f67dc37d14
shandilyaaryan <aryanshandilya10@gmail.com>
2025-01-13 22:39:09 +0530

    UTH Changes

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { ConvexReactClient, Authenticated, Unauthenticated, AuthLoading } from "convex/react";
+import {
+  ConvexReactClient,
+  Authenticated,
+  Unauthenticated,
+  AuthLoading,
+} from "convex/react";
 import { ReactNode } from "react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth, SignIn } from "@clerk/nextjs";
@@ -9,17 +14,25 @@ import { FullscreenLoader } from "./fullscreen-loader";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return  <ClerkProvider appearance={{ variables: { colorPrimary: 'black' }}} publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
-              <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-                <Authenticated>
-                  {children}
-                </Authenticated>
-                <Unauthenticated>
-                  <div className="flex flex-col items-center justify-center min-h-screen">
-                    <SignIn routing="hash" /> {/*TODO: Add main page*/}
-                  </div>
-                </Unauthenticated>
-                <AuthLoading><FullscreenLoader label="Loading..." /></AuthLoading>
-              </ConvexProviderWithClerk>
-          </ClerkProvider>;
+  return (
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "black" },
+        layout: { unsafe_disableDevelopmentModeWarnings: true },
+      }}
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <Authenticated>{children}</Authenticated>
+        <Unauthenticated>
+          <div className="flex flex-col items-center justify-center min-h-screen">
+            <SignIn routing="hash" /> {/*TODO: Add main page*/}
+          </div>
+        </Unauthenticated>
+        <AuthLoading>
+          <FullscreenLoader label="Loading..." />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
