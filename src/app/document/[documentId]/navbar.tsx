353206322ca8467bccff72c48b4e546815a1b4a0
shandilyaaryan <aryanshandilya10@gmail.com>
2025-01-13 20:18:30 +0530

    Miscellaneous Changes

--- src/app/document/[documentId]/navbar.tsx
+++ src/app/document/[documentId]/navbar.tsx
@@ -39,13 +39,34 @@ import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Avatars } from "./avatars";
 import Inbox from "./inbox";
 import { Doc } from "../../../../convex/_generated/dataModel";
+import { RenameDialog } from "@/components/rename-dialog";
+import { RemoveDialog } from "@/components/remove-dialog";
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface NavbarProps {
   data: Doc<"documents">;
 }
 
 export const Navbar = ({ data }: NavbarProps) => {
+  const router = useRouter();
   const { editor } = useEditorStore();
+
+  const mutation = useMutation(api.documents.create);
+
+  const onNewDocument = () => {
+    mutation({
+      title: "Untitled Document",
+      initialContent: "",
+    })
+      .catch(() => toast.error("Something went wrong"))
+      .then((id) => {
+        toast.success("Document created successfully");
+        router.push(`/document/${id}`);
+      });
+  };
 
   const insertTable = ({ rows, cols }: { rows: number; cols: number }) => {
     editor
@@ -132,19 +153,29 @@ export const Navbar = ({ data }: NavbarProps) => {
                       </MenubarItem>
                     </MenubarSubContent>
                   </MenubarSub>
-                  <MenubarItem>
+                  <MenubarItem onClick={onNewDocument}>
                     <FilePlusIcon className="size-4 mr-2" />
                     New Document
                   </MenubarItem>
                   <MenubarSeparator />
-                  <MenubarItem>
-                    <FilePenIcon className="size-4 mr-2" />
-                    Rename
-                  </MenubarItem>
-                  <MenubarItem>
-                    <TrashIcon className="size-4 mr-2" />
-                    Remove
-                  </MenubarItem>
+                  <RenameDialog documentId={data._id} initialTitle={data.title}>
+                    <MenubarItem
+                      onClick={(e) => e.stopPropagation()}
+                      onSelect={(e) => e.preventDefault()}
+                    >
+                      <FilePenIcon className="size-4 mr-2" />
+                      Rename
+                    </MenubarItem>
+                  </RenameDialog>
+                  <RemoveDialog documentId={data._id}>
+                    <MenubarItem
+                      onClick={(e) => e.stopPropagation()}
+                      onSelect={(e) => e.preventDefault()}
+                    >
+                      <TrashIcon className="size-4 mr-2" />
+                      Remove
+                    </MenubarItem>
+                  </RemoveDialog>
                   <MenubarSeparator />
                   <MenubarItem onClick={() => window.print()}>
                     <PrinterIcon className="size-4 mr-2" />
--- src/components/remove-dialog.tsx
+++ src/components/remove-dialog.tsx
@@ -16,40 +16,51 @@ import { toast } from "sonner";
 import { useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 interface RemoveDialogProps {
-    documentId: Id<"documents">;
-    children: React.ReactNode
+  documentId: Id<"documents">;
+  children: React.ReactNode;
 }
 
 export const RemoveDialog = ({ documentId, children }: RemoveDialogProps) => {
-    const remove = useMutation(api.documents.removebyId);
-    const [isRemoving, setIsRemoving] = useState(false);
+  const router = useRouter();
+  const remove = useMutation(api.documents.removebyId);
+  const [isRemoving, setIsRemoving] = useState(false);
 
-    return (
-        <AlertDialog>
-            <AlertDialogTrigger asChild>
-                {children}
-            </AlertDialogTrigger>
-            <AlertDialogContent onClick={(e) => e.stopPropagation()}>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>Are you sure? </AlertDialogTitle>
-                    <AlertDialogDescription>This action cannot be undone. This will permanently delete your document.</AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel onClick={(e) => e.stopPropagation()}>Cancel</AlertDialogCancel>
-                    <AlertDialogAction disabled={isRemoving} onClick={(e) => {
-                        e.stopPropagation();
-                        setIsRemoving(true);
-                        remove({id: documentId})
-                            .catch(() => toast.error("Something went wrong"))
-                            .then(() => toast.success("Document successfully removed"))
-                            .finally(() => setIsRemoving(false))  
-                    }} >
-                        Delete
-                    </AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    )
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent onClick={(e) => e.stopPropagation()}>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure? </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            document.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={(e) => e.stopPropagation()}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction
+            disabled={isRemoving}
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsRemoving(true);
+              router.push("/")
+              remove({ id: documentId })
+                .catch(() => toast.error("Something went wrong"))
+                .then(() => {
+                  toast.success("Document successfully removed");
+                })
+                .finally(() => setIsRemoving(false));
+            }}
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 };
